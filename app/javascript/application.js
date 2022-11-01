5cef8e9c32780d1529306510d844c616c5ae69a2
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-11-01 11:16:52 +0000

    Add backend-frontend

---
+++ app/javascript/application.js
@@ -0,0 +1,15 @@
+import "@hotwired/turbo-rails";
+import "./controllers";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux/configureStore";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ db/migrate/20221101104624_add_messages.rb
@@ -0,0 +1,9 @@
+class AddMessages < ActiveRecord::Migration[7.0]
+  def change
+    create_table :messages do |t|
+      t.text :greeting
+
+      t.timestamps
+    end
+  end
+end
